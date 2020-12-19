29feff898bef2319329104b640843091049161a1
startbug <51115577+startbug@users.noreply.github.com>
2020-12-19 16:59:23 +0800

    完善

--- gulimall-product/src/test/java/com/ggs/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/ggs/gulimall/product/GulimallProductApplicationTests.java
@@ -1,14 +1,19 @@
 package com.ggs.gulimall.product;
 
+<<<<<<< HEAD
 import com.ggs.gulimall.product.entity.BrandEntity;
 import com.ggs.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+=======
+import org.junit.jupiter.api.Test;
+>>>>>>> 2ed043713065bde2f5c7793492317d75f1aa505f
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+<<<<<<< HEAD
     @Autowired
     private BrandService brandService;
 
@@ -19,5 +24,10 @@ class GulimallProductApplicationTests {
         brandService.save(brandEntity);
         System.out.println("保存成功");
     }
+=======
+	@Test
+	void contextLoads() {
+	}
+>>>>>>> 2ed043713065bde2f5c7793492317d75f1aa505f
 
 }
